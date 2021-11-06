8aae285f0086120fc9c527852a4c18c6b960313d
Jade Mark <88232304+jd022@users.noreply.github.com>
2021-11-06 16:48:26 +0800

    initial commits

---
+++ professor/home.php
@@ -0,0 +1,17 @@
+<?php
+include("../dbConnection.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <title>Document</title>
+</head>
+<body>
+  <h2>Professor Dashboard</h2>
+  <p>i dont wanna be alone</p>
+</body>
+</html>
